cffeaf2673dfa7383fd0480b0ddd6307ea6d5e09
unaibermudez <ProgramameEsta@384>
2021-09-20 19:02:53 +0200

    hola

--- src/org/eda/packlaboratorio1/main.java
+++ src/org/eda/packlaboratorio1/main.java
@@ -2,6 +2,7 @@ package org.eda.packlaboratorio1;
 
 public class main {
     public void objeto() {
-        System.out.print("dmmmmgfs");
+        System.out.print("dmmmmgfsdavidputofeo");
+
     }
 }
