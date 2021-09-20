2e740de0d65b76ef0f51a67a22eb2bb3780608c7
david <davidelorzag@gmail.com>
2021-09-20 19:44:34 +0200

    hola

--- src/org/eda/packlaboratorio1/main.java
+++ src/org/eda/packlaboratorio1/main.java
@@ -2,7 +2,7 @@ package org.eda.packlaboratorio1;
 
 public class main {
     public void objeto() {
-        System.out.print("111112222223333444");
+        System.out.print("11tdyhjgfdgh1112222223333444");
         que feo eres david hijo
 
     }
