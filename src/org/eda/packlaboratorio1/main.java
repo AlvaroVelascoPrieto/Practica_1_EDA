d0e7e6f9456d3af1f1bfc4ae897ab443eab61c85
unaibermudez <ProgramameEsta@384>
2021-09-20 19:27:27 +0200

    hola x4

--- src/org/eda/packlaboratorio1/main.java
+++ src/org/eda/packlaboratorio1/main.java
@@ -2,7 +2,7 @@ package org.eda.packlaboratorio1;
 
 public class main {
     public void objeto() {
-        System.out.print("111112222223333");
+        System.out.print("111112222223333444");
 
     }
 }
