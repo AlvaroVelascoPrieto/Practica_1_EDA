f152eca59818e03cf8ac3451315843819cbc0df3
alvar <alvaritovelasco2002@gmail.com>
2021-09-21 12:12:45 +0200

    Añadidos algunos atributos, la constructora y el patron singleton para la clase ListaPeliculas

--- src/org/eda/packlaboratorio1/ListaPeliculas.java
+++ src/org/eda/packlaboratorio1/ListaPeliculas.java
@@ -1,4 +1,28 @@
 package org.eda.packlaboratorio1;
 
+
+import java.util.HashMap;
+
 public class ListaPeliculas {
+    private HashMap<String,Pelicula> peliculas;
+    private ListaActores todos;
+    private static ListaPeliculas miLista;
+
+    //Constructora
+    private ListaPeliculas(){
+        this.peliculas = new HashMap<String,Pelicula>();
+        this.todos = new ListaActores();
+    }
+
+    //Patron singleton que devuelve la unica instancia de ListaPeliculas
+    public static ListaPeliculas getListaPeliculas() {
+        if(ListaPeliculas.miLista==null) {
+            ListaPeliculas.miLista = new ListaPeliculas();
+        }
+        return miLista;
+    }
+
+    //Métodos
+
+
 }
