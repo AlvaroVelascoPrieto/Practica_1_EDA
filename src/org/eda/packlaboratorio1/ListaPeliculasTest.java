9f876e0e8fdfbd21339f8608cc47882aab591b00
david <davidelorzag@gmail.com>
2021-10-13 15:33:20 +0200

    en la clase actor solo he añadido la cabecera del apartado de otros metoodos y en la clase listaactors he añadido los metodos insetar borra y ordenar actores y he implementado los dos primeros a falta de añadir su respectiva parte en pelicula o lista peliculasç He añadido en ListaPeliculasTest nuevas clases y las he acabado queda el metodo devolverPeliculas y devolverActoresOrdenados(leer este metodo)

--- src/org/eda/packlaboratorio1/ListaPeliculasTest.java
+++ src/org/eda/packlaboratorio1/ListaPeliculasTest.java
@@ -9,6 +9,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.file.attribute.DosFileAttributeView;
+
 public class ListaPeliculasTest {
 
     Actor a1;
@@ -29,9 +31,9 @@ public class ListaPeliculasTest {
         a1 = new Actor("Alberto");
         a2 = new Actor("David");
         a3 = new Actor("Alvaro");
-        p1 = new Pelicula(1000, la1, "Lo imposible");
-        p2 = new Pelicula(1500, la2, "Oblivion");
-        p3 = new Pelicula(500, la3, "Harry Potter");
+        p1 = new Pelicula("Lo imposible", 2500);
+        p2 = new Pelicula("Oblivion", 5000);
+        p3 = new Pelicula("Harry Potter", 1000);
         lp1 = ListaPeliculas.getListaPeliculas();
         la1 = new ListaActores();
         la2 = new ListaActores();
@@ -62,45 +64,48 @@ public class ListaPeliculasTest {
 
     @Test
     public void testBuscarActor() {
-        assertEquals(lp1.buscarActor(a1.getNombre()), a1.nombre);
+        assertEquals(lp1.buscarActor(a1.getNombre()), a1);
     }
 
     @Test
     public void testIncrementarRecaudacion() {
-        assertEquals(lp1.incrementarRecaudacion(p1.nombre, 500), 1500);
+        assertEquals(lp1.incrementarRecaudacion(p1.nombre, 500), 3000);
     }
 
     @Test
     public void testInsertarActor() {
-        lp1.insertarActor(a1);
-        lp1.insertarActor(a2);
-        lp2.insertarActor(a1);
-        lp2.insertarActor(a2);
-        lp2.insertarActor(a3);
 
-        assertEquals(lp2.todos, lp1.insertarActor(a3.getNombre()));
+        la1.insertarActor("David");
+        p1.insertarActor("David");
+        ListaActores uno = lp1.insertarActor("Oblivion", "David")
 
+        assertEquals(la1, uno.devolverActores());
+        asserEquals(p1.devolverActores(), uno.devolverActores());
     }
 
     @Test
     public void testBorrarActor() {
-        lp1.insertarActor(a1);
-        lp1.insertarActor(a2);
-        lp1.insertarActor(a3);
-        lp2.insertarActor(a1);
-        lp2.insertarActor(a2);
+        lp1.insertarActor("Harry Potter", "Alberto");
+        lp1.insertarActor("Harry Potter", "Alvaro");
+        ListaActores uno = lp1.borrarActor("Oblivion", "Alberto");
+        la1.insertarActor("Alvaro");
+        p1.insertarActor("Alvaro");
 
-
-        assertEquals(lp2.todos, lp1.borrarActor(a3.getNombre()));
+        assertEquals(la1, uno.devolverActores());
+        asserEquals(p1.devolverActores(), uno.devolverActores());
     }
 
     @Test
     public void testDevolverActores() {
-        lp1.insertarActor(a1);
-        lp1.insertarActor(a2);
-        lp1.insertarActor(a3);
+        lp1.insertarActor("Oblivion", "Alberto");
+        lp1.insertarActor("Oblivion", "Alvaro");
+        lp1.insertarActor("Oblivion", "David");
 
-        assertEquals(lp1.devolverActores(pPelicula), );
+        la1.insertarActor( "Alberto");
+        la1.insertarActor( "Alvaro");
+        la1.insertarActor("David");
+
+        assertEquals(la1.devolverActores(), lp1.devolverActores(Oblivion))
     }
 
     @Test
@@ -109,6 +114,16 @@ public class ListaPeliculasTest {
 
     @Test
     public void testDevolverTodosLosActoresOrdenados() {
+        lp1.insertarActor("Oblivion", "Alberto");
+        lp1.insertarActor("Oblivion", "Alvaro");
+        lp1.insertarActor("Oblivion", "David");
+
+        lp1.insertarActor("Oblivion", "David");
+        lp1.insertarActor("Oblivion", "Alberto");
+        lp1.insertarActor("Oblivion", "Alvaro");
+
+        //Me faltaría un metodo borrar primerActor para hacer un assertEQuals entre lp1 y lp2 despues de haber sido ordenada lp2
+
     }
 
 }
